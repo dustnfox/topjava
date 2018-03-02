305439f04c9c9eee33db901421bd043041b26e7e
Mikhail Rassadin <dustnfox@gmail.com>
2018-03-02 14:37:23 +0300

    Add MealServlet

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,14 @@
+package ru.javawebinar.topjava.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MealServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        request.setAttribute("meals", null);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+    }
+}
