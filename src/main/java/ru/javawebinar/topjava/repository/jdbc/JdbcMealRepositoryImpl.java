85f5c79efd4b8fac226e181df0828e34d6815541
Mikhail Rassadin <dustnfox@gmail.com>
2018-03-16 16:03:07 +0300

    HW03_3 Implement meal save method

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,18 +1,49 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    private final SimpleJdbcInsert insertMeal;
+
+    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        insertMeal = new SimpleJdbcInsert(dataSource).withTableName("meals").usingGeneratedKeyColumns("id");
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource()
+                .addValue("id", meal.getId())
+                .addValue("date", meal.getDateTime())
+                .addValue("calories", meal.getCalories())
+                .addValue("description", meal.getCalories())
+                .addValue("user_id", userId);
+
+        if (meal.isNew()) {
+            Number id = insertMeal.executeAndReturnKey(map);
+            meal.setId(id.intValue());
+        } else if (namedParameterJdbcTemplate.update("UPDATE meals " +
+                "SET date=:date, calories=:calories, date=:date, description=:description " +
+                "WHERE id=:id AND user_id=:user_id", map) == 0) {
+            return null;
+        }
+        return meal;
     }
 
     @Override
