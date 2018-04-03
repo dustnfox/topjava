74b56696b2803d1a1977de499705f2c20e9b2ded
Mikhail Rassadin <dustnfox@gmail.com>
2018-04-03 15:07:48 +0300

    HW05_1 Implement DataJpaMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,30 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+    int deleteByIdAndUser(@Param("id") Integer id, @Param("userId") Integer userId);
+
+    Optional<Meal> findByIdAndUserId(Integer id, Integer userId);
+
+    List<Meal> findAllByUserIdOrderByDateTimeDesc(Integer userId);
+
+    List<Meal> findAllByUserIdAndDateTimeBetweenOrderByDateTimeDesc(Integer userId,
+                                                                    LocalDateTime startDate,
+                                                                    LocalDateTime endDate);
 }
